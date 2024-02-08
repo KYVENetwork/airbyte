b8eb35fd15391febf6b5c916e8e55626979af3f5
christopherbrumm <christopherbrumm2000@gmail.com>
2024-02-08 14:39:26 +0100

    chore: fix setup.py.

--- airbyte-integrations/connectors/source-kyve/setup.py
+++ airbyte-integrations/connectors/source-kyve/setup.py
@@ -7,13 +7,13 @@ from setuptools import find_packages, setup
 
 MAIN_REQUIREMENTS = [
     "airbyte-cdk~=0.2",
+    "Pympler==1.0.1"
 ]
 
 TEST_REQUIREMENTS = [
     "requests-mock~=1.9.3",
     "pytest~=6.2",
     "pytest-mock~=3.6.1",
-    "Pympler==1.0.1"
 ]
 
 setup(
